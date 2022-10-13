148e95c700515fae8fa49653d0135bdd73c9f86d
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-10-12 23:06:25 -0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -8,7 +8,7 @@ from twilio.rest import Client
 # account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # auth_token = os.environ['TWILIO_AUTH_TOKEN']
 account_sid = "ACc694da8ac49970a09c99ff6a81f6b872"
-auth_token = "aa91f359a3fd7ccbdc6e6388e76828d9"
+auth_token = "69e27d55c9c4d94059e3296f55aa49d5"
 
 client = Client(account_sid, auth_token)
 
