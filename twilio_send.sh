9ca1dd2cda4d364b82d237af6c12f2f8aa124960
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-10-12 22:51:21 -0400

    Update twilio_send.sh

--- twilio_send.sh
+++ twilio_send.sh
@@ -1,8 +1,7 @@
-#! /bin/bash
-$BODY="Body=$2"
+#! /bin/bash/
 
 curl -X POST https://api.twilio.com/2010-04-01/Accounts/ACc694da8ac49970a09c99ff6a81f6b872/Messages.json \
-                --data-urlencode $BODY \
+                --data-urlencode "Body=testing" \
                 --data-urlencode "From=+12137725021" \
                 --data-urlencode "To=+1$1" \
                 -u ACc694da8ac49970a09c99ff6a81f6b872:aa91f359a3fd7ccbdc6e6388e76828d9
